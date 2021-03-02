0f9a69d85af33453e5ed9b085f9ffcac7979a4ee
Nicholas Mercer <nick.mercer@fueltravel.com>
2021-03-01 22:54:16 -0500

    feat(release): v1.0.0

---
+++ _includes/header.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return <header>
+      <div>
+      <Link href='/'><a>Home</a></Link> - 
+      Example Header
+      </div>
+      </header>
+}
